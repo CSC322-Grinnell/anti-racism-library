2c39747bb3bb8963d54c30882f080e8377c22e8a
mikekben <benmikek@gmail.com>
2021-03-10 16:01:53 -0500

    Added users console page

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,31 @@
+class AdminController < ApplicationController
+    before_action :check_admin, except: [:index, :show, :search, :filter]   
+
+    def check_admin
+        if current_user == nil or !current_user.admin
+          redirect_to '/items/not_admin'
+        end
+    end 
+
+
+    def user_console
+        @everyone = User.all
+
+        @admins = @everyone.where(admin: true)
+        @users = @everyone.where(admin: false)
+    end
+
+    def promote
+        @user = User.find(params[:id])
+        @user.update_attribute(:admin, true)
+    
+        redirect_to :action => 'user_console'
+    end
+
+    def demote
+        @user = User.find(params[:id])
+        @user.update_attribute(:admin, false)
+    
+        redirect_to :action => 'user_console'
+    end
+end
