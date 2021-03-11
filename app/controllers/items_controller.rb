be6ad822e0c9c25bb9d300f904e35f6fd3897af4
nhBrgss <burgessn@grinnell.edu>
2021-03-10 19:18:31 -0800

    Added not_admin to admin before check exceptions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   #This controller's function is for the items that are stored in the library database. It has operations that create, delete,
   #and edit the items
 
-  before_action :check_admin, except: [:new, :index, :show, :search, :filter]         #WIP must verify that the user is an admin in order for to run any procudure in this controller. An admin
+  before_action :check_admin, except: [:new, :index, :show, :search, :filter, :not_admin]         #WIP must verify that the user is an admin in order for to run any procudure in this controller. An admin
   before_action :check_user, only: [:new]
                                               #should be the only user that is able to add/edit/delete items in the database
 
